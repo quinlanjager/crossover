2505e340cedc052df48ffb33a9e34fd9c499e3b8
Quinlan Jager <quinlan.jager@gmail.com>
2018-06-01 23:17:33 -0700

    Update the way we fetch character ids

--- lib/marvel_api.ex
+++ lib/marvel_api.ex
@@ -1,92 +1,84 @@
 defmodule Crossover.MarvelAPI do
-	use GenServer
-	use HTTPoison.Base
+  use GenServer
+  use HTTPoison.Base
 
-	@endpoint "http://gateway.marvel.com"
+  @endpoint "http://gateway.marvel.com"
 
-	# Interface functions
-	def get_series_with(characters) do
-		{:ok, pid} = start_genserver()
-		# loop over each character name, put their ids in the state
-		
-		# get series with their ids
-		
+  # Interface functions
+  def get_series_with(characters) do
+    {:ok, pid} = start_genserver()
+    # loop over each character name, put their ids in the state
+    character_ids = characters |> Enum.map(fn(character) -> get_character_id(pid, character) end)
+    character_ids |> Enum.join(",")
+    # series
+    # |> JSON.decode
+    # |> Enum.reduce
+    
+    # get series with their ids
 
-		# kill server once this job is completed
-		# stop(pid)
+    # kill server once this job is completed
+    # stop(pid)
 
-		# return series
-	end
+    # return series
+  end
 
-	# Client functions
-	defp start_genserver(opts \\ []) do 
-		GenServer.start_link(__MODULE__, :ok, opts)
-	end
-	
-	def get_character_from_state(pid, character_name) do
-		GenServer.call(pid, {:get, character_name})
-	end
-	
-	defp add_character_to_state(pid, character_name) do
-		GenServer.cast(pid, {:add, character_name})
-	end
-	
-	# GenServer callbacks
-	def init(args) do
-		{:ok, args}
-	end
-	
-	def handle_cast({:add, character_name}, _from, state) do
-		response_from_marvel = get_character_from_api(character_name)
-		
-		case response_from_marvel do
-			{:ok, %HTTPoison.Response{body: character}} -> 
-				updated_state = character 
-				|> parse_character_from_json 
-				|> prepend_item_to_state(state)
+  # Client functions
+  defp start_genserver(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
 
-				{:noreply, updated_state}
-			
-			{:error, reason} ->
-				{:error, reason}
-		end
+  def get_character_id(pid, character_name) do
+    GenServer.call(pid, {:get, character_name})
+  end
 
-	end
+  # GenServer callbacks
+  def init(args) do
+    {:ok, args}
+  end
 
-	def handle_call({:get, character_name}, _from, state) do
-		state |> Enum.find(fn(character) -> character[:name] == character_name end)
-	end
+  def handle_call({:get, character_name}, _from, state) do
+    response_from_marvel = get_character_from_api(character_name)
 
-	# HTTPoison callbacks
-	def process_url(url) do
-		@endpoint <> url
-	end
+    case response_from_marvel do
+      {:ok, %HTTPoison.Response{status_code: 200, body: response_from_marvel}} ->
+        character_id = response_from_marvel |> filter_character_id
+        new_state = [character_id | state]
+        {:reply, character_id, new_state}
 
-	# API functions
-	def get_character_from_api(character_name) do
-		get("/v1/public/characters?name=#{character_name}&" <> make_auth_signature())
-	end
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
 
-	def get_series_with_characters_from_api(character_ids) do
-		joined_list = character_ids |> Enum.join(",")
-		get("/v1/public/series?characters=#{joined_list}&" <> make_auth_signature())
-	end
+  # HTTPoison callbacks
+  def process_url(url) do
+    @endpoint <> url
+  end
 
-	# helper functions
-	defp parse_character_from_json(character) do
-		{:ok, JSON.decode(character)}
-	end
+  # API functions
+  def get_character_from_api(character_name) do
+    get("/v1/public/characters?name=#{character_name}&" <> make_auth_signature())
+  end
 
-	def prepend_item_to_state(item, state) do
-		[ item | state ]
-	end
+  def get_series_with_characters_from_api(character_ids) do
+    joined_list = character_ids |> Enum.join(",")
+    get("/v1/public/series?characters=#{joined_list}&" <> make_auth_signature())
+  end
 
-	defp make_auth_signature do
-		time_stamp = DateTime.utc_now() |> DateTime.to_time |> to_string
-		api_key = Application.fetch_env!(:crossover, :api_key)
-		secret_key = Application.fetch_env!(:crossover, :secret_key)
-		hash = :crypto.hash(:md5, time_stamp <> secret_key <> api_key) |> Base.encode16(case: :lower)
-		
-		"ts=#{URI.encode(time_stamp)}&apikey=#{api_key}&hash=#{hash}"
-	end
+  # helper functions
+  
+  defp filter_character_id(response_from_marvel) do
+    {:ok, character_info} = JSON.decode(response_from_marvel)
+   	{:ok, first_character_result} = character_info["data"]["results"] |> Enum.fetch(0)
+   	first_character_result["id"]
+  end
+
+  defp make_auth_signature do
+    time_stamp = DateTime.utc_now() |> DateTime.to_time() |> to_string
+    api_key = Application.fetch_env!(:crossover, :api_key)
+    secret_key = Application.fetch_env!(:crossover, :secret_key)
+    hash = :crypto.hash(:md5, time_stamp <> secret_key <> api_key) |> Base.encode16(case: :lower)
+
+    "ts=#{URI.encode(time_stamp)}&apikey=#{api_key}&hash=#{hash}"
+  end
 end
